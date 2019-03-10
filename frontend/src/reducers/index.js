5cc94cce367bdbc80a159bc8be8a18c511bf8eb9
Ilya Plenne <libbkmz.dev@gmail.com>
2019-03-10 18:46:34 +0300

    removed undefined reducer action. updated reducers state manipulation

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -4,20 +4,11 @@ import {OPEN_SIGNUP_MODAL, CLOSE_SIGNUP_MODAL} from "../actions";
 
 
 const rootReducer = createReducer(initialState, {
-        [ADD_NEW_ITEM]: (state, action) => {
-            return {
-                ...state,
-                test_items: [...state.test_items, action.payload]}
-        },
         [OPEN_SIGNUP_MODAL]: (state, action) => {
-            let out = state;
-            out.signup.visible = true;
-            return out;
+            return Object.assign({}, state, {signup: {visible: true}});
         },
         [CLOSE_SIGNUP_MODAL]: (state, action) => {
-            let out = state;
-            out.signup.visible = false;
-            return out;
+            return Object.assign({}, state, {signup: {visible: false}});
         }
 
     }
